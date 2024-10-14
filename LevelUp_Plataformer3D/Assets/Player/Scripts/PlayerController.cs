0184eaf5fc812509116133c02bddd6c61edd52b0
28victor <victorescos.b@gmail.com>
2024-10-14 23:45:24 +0200

    v0.1

---
+++ LevelUp_Plataformer3D/Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Parameters")]
+    [SerializeField] private float movementSpeed = 4f;
+    [SerializeField] private float orientationSpeed = 360f;
+    [SerializeField] private float jumpSpeed = 7.5f;
+    
+    [Header("Inputs")]
+    [SerializeField] private InputAction movement;
+    [SerializeField] private InputAction jump;
+
+    private Camera mainCamera;
+    private CharacterController characterController;
+
+    private float verticalVelocity = 0f;
+    [SerializeField] private float gravity = -9.8f; // m/s2
+    private void OnEnable()
+    {
+        movement.Enable();
+        jump.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movement.Disable();
+        jump.Disable();
+    }
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        characterController = GetComponent<CharacterController>();
+        
+    }
+
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+        UpdateVerticalMovement();
+        UpdateMovement(out var movementValue, out var movementValueXZ);
+        UpdateOrientation(movementValue, movementValueXZ);
+    }
+    
+    private void UpdateVerticalMovement()
+    {
+        //Update jump
+        if ((jump.ReadValue<float>() > 0f) && (characterController.isGrounded))
+        {
+            verticalVelocity = jumpSpeed;
+        }
+        else
+        {
+            //Update gravity
+            if (characterController.isGrounded) {verticalVelocity = 0f;}
+            
+            // m/s                  += m/(s*s)  *       s       ;
+            verticalVelocity += gravity * Time.deltaTime;
+            
+        }
+    }
+    
+    private void UpdateMovement(out Vector2 movementValue, out Vector3 movementValueXZ)
+    {
+        //Detectar input
+        movementValue = movement.ReadValue<Vector2>();
+        movementValueXZ = new Vector3(movementValue.x, 0f, movementValue.y);
+        
+        //Mover jugador respecto camara
+        float oldMagnitudeXZ = movementValueXZ.magnitude;
+        movementValueXZ = mainCamera.transform.TransformDirection(movementValueXZ);
+        movementValueXZ = Vector3.ProjectOnPlane(movementValueXZ, Vector3.up);
+        movementValueXZ = movementValueXZ.normalized * oldMagnitudeXZ;
+        
+        Vector3 velocityXZ = movementValueXZ * movementSpeed;
+        Vector3 velocityY = (Vector3.up * verticalVelocity);
+        Vector3 velocity = velocityXZ + velocityY;
+        
+        //Mover jugador con Input System
+        characterController.Move(velocity * Time.deltaTime);
+    }
+    
+    private void UpdateOrientation(Vector2 movementValue, Vector3 movementValueXZ)
+    {
+        //Orientar jugador respecto camara
+        if (movementValue.magnitude > 0.01f)
+        {
+            //Usamos el mainCamera si queremos que el jugador se oriente siempre mirando hacia enfrente
+            //Vector3 desiredForward = mainCamera.transform.forward;
+            Vector3 desiredForward = movementValueXZ;
+            desiredForward = Vector3.ProjectOnPlane(desiredForward, Vector3.up).normalized;
+            Vector3 currentForward = transform.forward;
+            float angleDifference = Vector3.SignedAngle(currentForward, desiredForward, Vector3.up);
+            float angleToApply = Math.Min(Mathf.Abs(angleDifference), orientationSpeed * Time.deltaTime);
+            angleToApply *= Mathf.Sign(angleDifference);
+        
+            Quaternion rotationToApply = Quaternion.AngleAxis(angleToApply, Vector3.up);
+            transform.rotation = rotationToApply * transform.rotation;
+        }
+    }
+}
